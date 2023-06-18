ea080fe60e9dacfed3bec617bc2cc6a2336b1519
LeKeu <lekeuffer@gmail.com>
2023-06-17 22:50:31 -0300

    Criação de serviçoes, faltam pequenos ajustes

--- Ultimo/Controllers/CartaoController.cs
+++ Ultimo/Controllers/CartaoController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Ultimo.Models;
 using Ultimo.Services;
+using Microsoft.AspNetCore.Mvc;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Ultimo.Controllers
 {
@@ -10,10 +12,33 @@ namespace Ultimo.Controllers
     public class CartaoController : ControllerBase
     {
         private readonly CartoesService _cartoesService;
+        Extras extras;
 
         public CartaoController(CartoesService cartoesService)
         {
             _cartoesService = cartoesService;
+        }
+
+        private bool ChecarSenha(Cartao newCartao)
+        {
+            var tudoJunto = "" + (newCartao.DataNasc.Value.Year.ToString().Substring(2)) + "" +
+                (newCartao.DataNasc.Value.Month.ToString().Length == 1 ? ("0" + newCartao.DataNasc.Value.Month) : (newCartao.DataNasc.Value.Month))
+                + "" + (newCartao.DataNasc.Value.Day);
+
+            var idade = DateTime.Today.Year - newCartao.DataNasc.Value.Year;
+            var senha = newCartao.Senha.ToString();
+            var senhaConfirm = newCartao.SenhaConfirm.ToString();
+
+            //System.Diagnostics.Debug.WriteLine(extras.isConsecutive(senha));
+            if (idade < 18)
+
+
+            if (idade >= 18 && senha.Length == 6 && senha != tudoJunto){
+                System.Diagnostics.Debug.WriteLine("Senha Linda");
+                return true;
+            }
+
+            return false;
         }
 
         [HttpGet]
@@ -32,13 +57,29 @@ namespace Ultimo.Controllers
             return cartao;
         }
 
-        [HttpPost]
+        [HttpPost("SolicitarCartão")]
         public async Task<IActionResult> Post(Cartao newCartao)
         {
+            var senha_ok = ChecarSenha(newCartao);
+            string aviso = null;
+
+            if(DateTime.Today.Year - newCartao.DataNasc.Value.Year < 18)
+                return BadRequest("É obrigatório ter 18 ou mais de idade para solicitar um cartão.");
+
+            if (newCartao.Bandeira != "Mastercard" && newCartao.Bandeira != "Visa")
+                aviso += "É necessário que a Bandeira seja 'Mastercard' ou 'Visa'.\n";
+
+            if (newCartao.DataVenc != "5" && newCartao.DataVenc != "10" && newCartao.DataVenc != "15" && newCartao.DataVenc != "20")
+                aviso += "É necessário que a Data de Vencimento seja '5', '10', '15' ou '20'.\n";
+
+            if (newCartao.Tipo != "PLATINUM" && newCartao.Tipo != "GOLD" && newCartao.Tipo != "BLACK" && newCartao.Tipo != "DIAMOND")
+                aviso += "É necessário que o Tipo seja 'PLATINUM', 'GOLD', 'BLACK' ou 'DIAMOND'.";
+
             await _cartoesService.CreateAsync(newCartao);
+            if (!string.IsNullOrEmpty(aviso))
+                return BadRequest(aviso);
 
             return CreatedAtAction(nameof(Get), new {id = newCartao.Id}, newCartao);
-
         }
 
         [HttpPut("{id:length(24)}")]
@@ -56,6 +97,51 @@ namespace Ultimo.Controllers
             await _cartoesService.UpdateAsync(id, updateCartao);
 
             return NoContent();
+        }
+
+        //ATIVAÇÃO DE CARTÃO
+        [HttpPut("AtivarCartao")]
+        public async Task<IActionResult> AtivarCartao(string id, string agencia, string conta, string senha)
+        {
+            var cartao = await _cartoesService.GetAsync(id);
+
+            cartao.Id = cartao.Id;
+
+            if (cartao.Agencia.ToString() == agencia && cartao.Conta.ToString() == conta && cartao.Senha.ToString() == senha)
+            {
+                System.Diagnostics.Debug.WriteLine("ATIVO");
+                cartao.Status = "ATIVO";
+
+                await _cartoesService.UpdateAsync(id, cartao);
+
+                return CreatedAtAction(nameof(Get), new { id = cartao.Id }, cartao);
+            }
+            else
+            {
+                return BadRequest("Cartão não encontrado. Cheque se as informações estão corretas.");
+            }
+        }
+
+        //BLOQUEAR CARTÃO
+        [HttpPut("BloquearCartao")]
+        public async Task<IActionResult> BloquearCartao(string id, string agencia, string conta, string senha, string motivo)
+        {
+            var cartao = await _cartoesService.GetAsync(id);
+
+            cartao.Id = cartao.Id;
+
+            if (cartao.Agencia.ToString() == agencia && cartao.Conta.ToString() == conta 
+                && cartao.Senha.ToString() == senha && cartao.Status == "ATIVO")
+            {
+                cartao.Status = motivo;
+                await _cartoesService.UpdateAsync(id, cartao);
+                return CreatedAtAction(nameof(Get), new { id = cartao.Id }, cartao);
+            }
+            else
+            {
+                return BadRequest("Cartão não encontrado. Cheque se as informações estão corretas.");
+            }
+
         }
 
         [HttpDelete("{id:length(24)}")]
--- Ultimo/Models/Cartao.cs
+++ Ultimo/Models/Cartao.cs
@@ -10,5 +10,18 @@ namespace Ultimo.Models
         public string? Id { get; set; }
         public string? Conta { get; set; }
         public string? Agencia { get; set; }
+        public string? Cpf { get; set; }
+        public DateTime? DataNasc { get; set; }
+        public string? NomeCompleto { get; set; }
+        public string? NomeCartao { get; set; }
+        public string? Bandeira { get; set; }
+        public string? Tipo { get; set; }
+        public string? DataVenc { get; set; }
+
+        public string? Senha { get; set; }
+        public string? SenhaConfirm { get; set; }
+        public string? Status { get; set; } = "SOLICITADO";
+
+
     }
 }
